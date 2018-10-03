c172eb7838f2a590f1630bbde435e8902341fe75
Emerson <emermarti82@gmail.com>
2018-10-03 16:07:51 -0400

    adding server list

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,13 +25,14 @@ ln99826.corp.ads, btln001649.corp.ads,btln001650.corp.ads,btln002025.corp.ads,bt
 btln002027.corp.ads,btln002028.corp.ads,btln007399.corp.ads,btln007401.corp.ads,btln007402.corp.ads,
 ln99694.corp.ads,ln99695.corp.ads,ln99696.corp.ads,ln99734.corp.ads,ln99735.corp.ads,ln99778.corp.ads,
 ln99779.corp.ads,ln99784.corp.ads,ln99785.corp.ads,ln99788.corp.ads,ln99833.corp.ads,ln99834.corp.ads,
-ln99835.corp.ads,btln000045.corp.ads,btln000046.corp.ads,btln001603.corp.ads,btln002029.corp.ads,
-btln002030.corp.ads,btln002041.corp.ads,btln007166.corp.ads,btln007255.corp.ads,btln007256.corp.ads,
-btln007530.corp.ads,btln007531.corp.ads,btln007571.corp.ads,btln007572.corp.ads,btln007573.corp.ads,
-ln98312.corp.ads,ln98315.corp.ads,ln98316.corp.ads,ln98317.corp.ads,ln98318.corp.ads,ln98321.corp.ads,
-ln98559.corp.ads,ln99231.corp.ads,ln99232.corp.ads,ln99233.corp.ads,ln99234.corp.ads,ln99254.corp.ads,
-ln99663.corp.ads,ln99664.corp.ads,ln99772.corp.ads,ln99773.corp.ads,ln99825.corp.ads,ln99826.corp.ads,
-ln99827.corp.ads {
+ln99835.corp.ads,btln000045.corp.ads,btln000046.corp.ads,btln000047.corp.ads,btln000048.corp.ads,
+btln001604.corp.ads,btln001605.corp.ads,btln002031.corp.ads,btln002032.corp.ads,btln002042.corp.ads,
+btln007253.corp.ads,btln007254.corp.ads,btln007528.corp.ads,btln007529.corp.ads,btln007568.corp.ads,
+btln007569.corp.ads,btln007570.corp.ads,ln98312.corp.ads,ln98319.corp.ads,ln98320.corp.ads,ln98321.corp.ads,
+ln98556.corp.ads,ln98557.corp.ads,ln99227.corp.ads,ln99228.corp.ads,ln99229.corp.ads,ln99230.corp.ads,
+ln99254.corp.ads,ln99665.corp.ads,ln99666.corp.ads,ln99667.corp.ads,ln99770.corp.ads,ln99771.corp.ads,
+ln99822.corp.ads,ln99823.corp.ads,ln99824.corp.ads,ln99147.corp.ads,ln99148.corp.ads,btln000011.corp.ads,
+btln000012.corp.ads {
 class { 'role::rl_dynatrace': }
 }
 
