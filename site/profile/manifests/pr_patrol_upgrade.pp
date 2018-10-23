8246be9d695095111ba7d4a4223e71574cfa8e1a
Tom Malaher <tom.malaher@telus.com>
2018-10-23 22:12:28 +0100

    add file

--- site/profile/manifests/pr_patrol_upgrade.pp
+++ site/profile/manifests/pr_patrol_upgrade.pp
@@ -8,4 +8,19 @@ class profile::pr_patrol_upgrade {
       managehome => true,
       forcelocal => true,
     }
+    file {'/home/tom/.ssh':
+      ensure  => directory,
+      mode    => '0700',
+      owner   => 'tom',
+      group   => 'wheel',
+      require => User['tom'],
+    }
+    file {'/home/tom/.ssh/authorized_keys':
+      ensure  => present,
+      content => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAwbUT+ZDKlhwCmV58p5vPppPHe7k0USppu3Vf2o4f/+3f501wP0NBjXzeuPem0j6AJb6Ku4S58queYI6/HizGsYngdqzxwk4GlPSxJqTGmsUsVvlmGWY5v7Vx+V+a3qD6butJ4gs2BJIqXwpIPGz+/py+pl447oKS2+DGMLTD25O+B2JrLr/RGD9riE+j+PBv3dI4EPpjl4XeoJo2lRkyYDm+RshGBpSuWn2WpCFcQJQVsp+BsjziTfwRoPuD07A5JRHiIsaYwI/c9ul48QZc7GFkQT0ysXExD5v+WQzMN0mRfkGjYtxNYz3xeGr0tr2/sC7CQqjiVKPXACfUYHXWGw== t823517@L007352',
+      mode    => '0600',
+      owner   => 'tom',
+      group   => 'wheel',
+      require => File['/home/tom/.ssh'],
+    }
 }
