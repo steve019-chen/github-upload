255cab588403b0f19abf95395364b65c7ef0e8b2
Tom Malaher <tom.malaher@telus.com>
2018-10-23 22:06:34 +0100

    tweak settings

--- site/profile/manifests/pr_patrol_upgrade.pp
+++ site/profile/manifests/pr_patrol_upgrade.pp
@@ -1,9 +1,11 @@
 # Profile for patrol upgrades
 class profile::pr_patrol_upgrade {
     user {'tom':
-      shell      => '/bin/bash',
+      ensure     => present,
+      shell      => '/bin/tcsh',
       password   => pw_hash(lookup('tom::app_account_password'), 'SHA-512','mysalt'),
-      require    => Group['dynatrace'],
+      gid        => 'wheel',
       managehome => true,
+      forcelocal => true,
     }
 }
