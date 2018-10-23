fc078446bd31c4c0d642dbb613390c21349da2fa
Tom Malaher <tom.malaher@telus.com>
2018-10-23 21:59:41 +0100

    rename "update" to "upgrade"

--- site/profile/manifests/pr_patrol_upgrade.pp
+++ site/profile/manifests/pr_patrol_upgrade.pp
@@ -1,5 +1,5 @@
 # Profile for patrol upgrades
-class profile::pr_patrol_update {
+class profile::pr_patrol_upgrade {
     user {'tom':
       shell      => '/bin/bash',
       password   => pw_hash(lookup('tom::app_account_password'), 'SHA-512','mysalt'),
