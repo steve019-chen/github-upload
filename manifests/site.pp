77c413fedbaf4ff92366b509bd3db31cfe0802e6
Daruvin Sood <daruvin.sood@telus.com>
2018-12-03 14:44:03 -0800

    merge conflict resolved for flexera commented out servers

--- manifests/site.pp
+++ manifests/site.pp
@@ -18,12 +18,6 @@ node default {
 
 # lint:ignore:unquoted_node_name lint:ignore:140chars
 
-<<<<<<< HEAD
-node btlp007087.corp.ads, btlp007088.corp.ads, btlp007089.corp.ads, btlp007090.corp.ads, btlp007091.corp.ads, btlp007092.corp.ads,
-btlp007093.corp.ads, btlp007094.corp.ads, btlp007095.corp.ads, btlp007096.corp.ads, btlp007097.corp.ads, btlp007098.corp.ads,
-btlp007099.corp.ads, btlp007100.corp.ads, btlp007101.corp.ads, btlp007102.corp.ads, btlp007103.corp.ads, btlp007104.corp.ads,
-btlp007105.corp.ads, btlp007106.corp.ads,daruvin1,daruvin2,daruvin3,daruvin4,daruvin5 {
-=======
 # Flexera testing
 
 node ln-test11 {
@@ -260,7 +254,6 @@ node ln-test11 {
 node btlp007087, btlp007088,btlp007089,btlp007090,btlp007091,btlp007092,btlp007093,btlp007094,btlp007095,btlp007096,
 btlp007097,btlp007098,btlp007099,btlp007100,btlp007101,btlp007102,btlp007103,btlp007104,btlp007105,btlp007106,
 btlp007107,btlp007108,btlp007109,btlp007110,btlp007111,btlp007112,btlp007113,btlp007114,btlp007115,btlp007116 {
->>>>>>> 6f4876fc246a319e84c8ac30df36efeed3b4fdd1
 class { 'role::rl_bcg_shannon': }
 }
 
