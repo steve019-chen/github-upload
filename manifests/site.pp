2ba5d9cd3f4d20b5072777f922926b8f02714280
Shaun McEvoy <Shaun.McEvoy@telus.com>
2019-01-09 12:06:59 -0800

    remove puppet agent config from default stanza

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
 
   # Puppet agent custom settings
-  class { 'puppet_agent_config::puppet_conf': }
+  # class { 'puppet_agent_config::puppet_conf': }
 
   # LVM module used for creating non-rootvg filesystems
   class { 'lvm': }
