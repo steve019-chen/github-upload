8afb71fae9ce64c7c20d752377fa65b876c3b2ed
Alex Lakusta <t838383@corp.ads>
2019-01-10 13:48:20 -0700

    1st Commit

--- site/profile/manifests/pr_base_win.pp
+++ site/profile/manifests/pr_base_win.pp
@@ -1,5 +1,5 @@
 class profile::pr_base_win {
-notify {"Managed by Puppet! Hello World! ":}
+notify {"Managed by Puppet! Hello World, this is Alex! ":}
 service { 'PatrolAgent':
 name => 'PatrolAgent',
 ensure => 'stopped',
