5f884e96c6cc6b7a1a0637e004a51a4331b5d683
Emerson <emermarti82@gmail.com>
2019-02-04 15:34:42 -0500

    deploying nginx

--- site/profile/manifests/pr_diversio.pp
+++ site/profile/manifests/pr_diversio.pp
@@ -3,11 +3,20 @@
 # This class installs and configures NGINX for Diversio project
 #
 
-class profile::pr_diversio {"nginx":}
-package { [
- 'nginx'
-  ]:
-  ensure   => 'v1.14.2 ',
-  provider => 'yum',
+class {'sudo':
+  purge               => false,
+  config_file_replace => false,
+  }
+
+class profile::pr_diversio {
+ package {"nginx":
+ ensure   => '1.14.2',
+ }
+
+# Include rules in “content”
+sudo::conf { 'puppet_nginx':
+  priority => 10,
+  content  => 'infra ALL=NOPASSWD : /opt/puppetlabs/bin/puppet agent *, /sbin/service nginx start, /sbin/service nginx stop, /sbin/service nginx restart, /sbin/service nginx status',
+  require  => User['infra'],
+  }
 }
-#
