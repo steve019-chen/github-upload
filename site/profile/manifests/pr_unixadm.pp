1f1f0e45135b4dca7d1410d430f07b28ad1140eb
Daruvin Sood <daruvin.sood@telus.com>
2019-05-02 14:01:36 -0700

    adding encrypted password and from option to limit to 1 ip

--- site/profile/manifests/pr_unixadm.pp
+++ site/profile/manifests/pr_unixadm.pp
@@ -1,11 +1,12 @@
-# Class: Role <insert name>
+# Class: Profile pr_unixadm
 #
 # This Role defines the profiles required by servers part of the <insert name / project>
 #
-# Parameters:
+# Parameters: No parameters
 #
 # Actions:
-#   - <insert text>
+#   - Creates 2 groups : unixt4 and remotelogin
+#   - Create 1 user : unixt4
 # 
 # Prereqs:
 #   - <insert text>
@@ -15,31 +16,40 @@ class profile::pr_unixadm {
 # For reference, as provided by Novo request
 # Account: unixt4 uid: 53535
 # Group: unixt4 gid: 53535
+# Approved Novo request: 247409
 
 # Create the users group
-
 group { 'unixt4':
   ensure => present,
   gid    => '53535',
 }
 
-# ToDo: Create an if statement to check if remotelogin is present
+# Create the remotelogin group
+group { 'remotelogin':
+  ensure => present,
+  gid    => '5050',
+}
+
 # Create the unixt4 user for application account, set password
 
 user { 'unixt4':
   uid        => '53535',
   gid        => 'unixt4',
   shell      => '/bin/bash',
-  groups     => 'remotelogin',	
-  password   => '*LK*',
+  groups     => 'remotelogin',
+  password   => pw_hash(lookup('unixt4::password'), 'SHA-512','mysalt'),
   managehome => true,
-  require    => Group['unixt4'],
+  require    => Group['unixt4','remotelogin'],
 }
+# lint:ignore:140chars
+# Create ssh key with the public key and limit connection from ln99052
 ssh_authorized_key { 'unixt4@ln99052':
   ensure => present,
   user   => 'unixt4',
   type   => 'ssh-rsa',
+  options => {'from' => '142.63.43.98'},
   key    => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA0Ou2HuF2wwGuLR3kZtco9K6LdIsFpoaLhNlIveLqoqnuYsCIwDIjwNawgep35B/koQyika6zcVY7SNsj5rSOzfpsoA6NvTxcNfdfOakCbBQCLQfza3P1EBuQutVagcYAuukyM14LRTACDKBSnV8b46AvW5DE9c1po6iIAq22dsNGIHcNn17CnXI9WA9fEy1S7+ioGPPjwOz+UkXzxaOr9InwO9/kxLBcBLfGfMIvYW8TPPXTGw8+VNwJg9g5ZodaMR6JKGccOxL+mj4EWTOc56s/diRarbkGky78I1eg7JyQWtDKNvAz7cQ1eANUdDa7LOSHDgfR8n5uDD4wp+tBqw==',
 }
+# lint:endignore
 
 }
