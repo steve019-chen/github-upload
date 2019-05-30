b2a9b41134cb13b9c14f5ef85c583460ee1c2330
Daruvin Sood <daruvin.sood@telus.com>
2019-05-30 16:38:56 -0700

    Seperating windows, linux base and limiting unixadm account to KIDC non prod

---
+++ site/profile/manifests/pr_base_win.pp
@@ -0,0 +1,14 @@
+# Class: pr_base_win
+#
+# This class is the base profile for TELUS Windows servers
+#
+# Actions:
+#   - None
+# 
+# Prereqs:
+#   - None
+#
+
+class profile::pr_base_win { 
+  
+}
