d1dd3d44c9758908f87e614ab438da83fca6235d
Emerson <emermarti82@gmail.com>
2019-07-16 16:18:38 -0400

    adding env variables to profile-2

--- site/profile/manifests/pr_dynatrace1agent.pp
+++ site/profile/manifests/pr_dynatrace1agent.pp
@@ -18,7 +18,7 @@
 ###
 #
 class profile::pr_dynatrace1agent (
-  Enum['common','dv-env','st-env','pr-env'] $enviroment = 'common',
+  Enum['common','dv-env','st-env','pr-env'] $environment = 'common',
 )
  {
 
@@ -56,12 +56,8 @@ class profile::pr_dynatrace1agent (
     # Calling the module and passing a download location and source for the installation file##
 
     class { 'dynatraceoneagent':
-<<<<<<< HEAD
         download_link => 'puppet:///software/dynatraceoneagent/$environment/Dynatrace-OneAgent-Linux-1.171.226.sh',
         #download_link => 'http://jty656.dynatrace-managed.com/e/56f21ab8-4f4b-4a14-9afb-bd493a8884ac/api/v1/deployment/installer/agent/unix/default/latest?Api-Token=ykJK2lC6S8eWmZwETfVYn&arch=x86&flavor=default',
-=======
-        download_link => 'puppet:///software/dynatraceoneagent/common/Dynatrace-OneAgent-Linux-1.167.176.sh',
->>>>>>> 22d4efa14898a108b26e58ce5b73506dafcd1645
         download_dir  => '/tmp',
         user          => 'dynatrace',
         require       => User['dynatrace'],
