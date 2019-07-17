ccc27c990a560b6e4015622fb0a84b757a31ec2c
Daruvin Sood <daruvin.sood@telus.com>
2019-07-17 14:26:21 -0700

    switching from include to class

--- site/role/manifests/rl_dynatrace_st.pp
+++ site/role/manifests/rl_dynatrace_st.pp
@@ -1,5 +1,5 @@
 #Role for adding Dynatrace One agent st
 class role::rl_dynatrace_st {
   include profile::pr_base
-  include profile::pr_dynatrace1agent{ environment = 'st-env': }
+  class { 'profile::pr_dynatrace1agent': environment => 'st-env', }
 }
