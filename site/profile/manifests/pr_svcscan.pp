0fbe1383fa8142e547041fbc703fdd9408ef87fb
Daruvin Sood <daruvin.sood@telus.com>
2019-11-25 11:28:40 -0800

    including 2nd ssh account for tools

--- site/profile/manifests/pr_svcscan.pp
+++ site/profile/manifests/pr_svcscan.pp
@@ -76,22 +76,36 @@ else
     require => User['svcscan'],
   }
   # lint:ignore:140chars
-  # Create ssh key with the public key and limit connection from ln99052
-  ssh_authorized_key { 'svcscan':
+  # # Limiting deployment to : KIDC Prod, KIDC NP, Toll, Laird, QIDC Non Prod, QIDC Prod
+  # # Not deploying to: QIDC Tools, KIDC Tools
+  if ($facts['puppet_server'] in ['btln007206.corp.ads','btln002494.corp.ads','btlp000336.corp.ads','btln000197.corp.ads','btlp000966.corp.ads','lp99604.corp.ads','lp99605.corp.ads'] )
+  {
+    # Create ssh key with the public key and limit connection to IPs below
+    ssh_authorized_key { 'svcscan':
+      ensure  => present,
+      user    => 'svcscan',
+      type    => 'ssh-rsa',
+      options => ['from="100.64.33.30,100.70.15.158,100.70.15.254,100.70.22.62,142.174.129.135,142.174.129.139,142.174.129.175,142.174.61.117,142.174.61.79,142.178.174.10,142.178.204.112,142.178.204.113,142.178.205.169,142.178.22.16,142.178.22.77,142.63.129.17,204.191.153.3,209.29.0.169,209.29.0.170,209.29.0.44,75.153.176.179,75.153.235.98,75.153.235.99,96.1.170.237,96.1.250.230"','no-agent-forwarding','no-port-forwarding','no-X11-forwarding'],
+      key     => 'AAAAB3NzaC1yc2EAAAABJQAAAQEAjHK8a6LfhEsrUuy5LxcmgFS++lzLNgnyBoQS8oRsG/Wb8vgbyJ9wW4wLxvTHIOvULsRoR5Q0zrI9uX7NVZug614iZf0qQ4yxrOgkz8Q+bZDO6pbJdFWPyqQP83N7e/1Pk1RULbH7tfe7bD7N0lG0HBy7NMh3rjqISTxB7zHbmM3HXbFFOJs3qqdxZ0foXNpeB9Ob1Plg02B6+fSZ+BeaVNC/zEgMwIxEcgpwq1cMigY+jFlFD6yaArr67wF7WQeArd6f/NY5jq+BEnqhEDp49Yul3naMvuamn52/q46+b4JtsWpL750/c8CRtSDg1LPsrb6jq7HrRK0saqc0o+okuw==',
+      require => File['/home/svcscan'],
+    }
+  }
+  # # Limiting deployment to : QIDC Tools, KIDC Tools
+  # # Not deploying to: KIDC Prod, KIDC NP, Toll, Laird, QIDC Non Prod, QIDC Prod
+  elsif ($facts['puppet_server'] in ['tslp000443.corptools.ads','tslp000578.corptools.ads'] )
+  {
+    ssh_authorized_key { 'svcscantools':
     ensure  => present,
     user    => 'svcscan',
     type    => 'ssh-rsa',
     options => ['from="100.64.33.30,100.70.15.158,100.70.15.254,100.70.22.62,142.174.129.135,142.174.129.139,142.174.129.175,142.174.61.117,142.174.61.79,142.178.174.10,142.178.204.112,142.178.204.113,142.178.205.169,142.178.22.16,142.178.22.77,142.63.129.17,204.191.153.3,209.29.0.169,209.29.0.170,209.29.0.44,75.153.176.179,75.153.235.98,75.153.235.99,96.1.170.237,96.1.250.230"','no-agent-forwarding','no-port-forwarding','no-X11-forwarding'],
-    key     => 'AAAAB3NzaC1yc2EAAAABJQAAAQEAjHK8a6LfhEsrUuy5LxcmgFS++lzLNgnyBoQS8oRsG/Wb8vgbyJ9wW4wLxvTHIOvULsRoR5Q0zrI9uX7NVZug614iZf0qQ4yxrOgkz8Q+bZDO6pbJdFWPyqQP83N7e/1Pk1RULbH7tfe7bD7N0lG0HBy7NMh3rjqISTxB7zHbmM3HXbFFOJs3qqdxZ0foXNpeB9Ob1Plg02B6+fSZ+BeaVNC/zEgMwIxEcgpwq1cMigY+jFlFD6yaArr67wF7WQeArd6f/NY5jq+BEnqhEDp49Yul3naMvuamn52/q46+b4JtsWpL750/c8CRtSDg1LPsrb6jq7HrRK0saqc0o+okuw==',
-    require => File['/home/svcscan'],
-  }
-  ssh_authorized_key { 'svcscan2':
-    ensure  => present,
-    user    => 'svcscan',
-    type    => 'ssh-rsa',
-    options => ['no-agent-forwarding','no-port-forwarding','no-X11-forwarding'],
     key     => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDC/4v0rMbQAOJIkdrq3DPW9e6yt8DKe6lwD16av32I05p/bl2xZg+XRVYC++/+80ir+41sRW/UVgQZyDK3tJXEy65DyUvbf2Iy4xPu9Xq0r/vH64JaLHWWtNvRlHoVj0uXLb4lLehUKvbJLAXCaPSf76JUuKtrizJnUdDT27+h+l4sq3aJgC41nmvDHtmVkQXKEDYZqK+kb39j6F3CFwU4jcMbBh0PGZQ7tkYWurA8JbUNS8VTpVEuTzZ1Z5U9gYKiUesQ35qr90YE1kTpZQeEG+Nvx/ht9F+RAGzDLDdKZmn0088gi5hF5qBDiC4eo9ahhknX3Tj6XF2EnLzHQAXk8T9qExNL+9V0PZXLlVP3GgHe0kkbpU+tjquaYZqAnS51dF+H/A0wXxVQv0Wd2AFEAnj88ZybET3OOmcIvQhOiRO8DjeZbBcQPC397Ysm3NNMvCVAZWwguwYFtmkGQOXnfjlbWFpgNPGprJM65lQ/r+WwNkxMNCNFNW+3Cl2JPXAyxnZTBGuoPZ8iFxyG557+FIUyQKEoix62SleQu+7S9Sui7Y13f3KYfYOrQgNJSj4DgqMCRYXXnCorWAJeJ+TNechn1lHQntT1QKsWs82aGryla8Ft89sujDzo3LmeY+2dxix0Nqq6poRY0mtiVuTbBCC4Fc2qb+i5vr9FpRKeFw==',
     require => File['/home/svcscan'],
+    }
+  }
+  else
+  {
+    # No service account
   }
   # lint:endignore
 
