75cfc38a6384923a3a0ef8716f81dca9977b78d7
Daruvin Sood <daruvin.sood@telus.com>
2019-11-29 09:55:07 -0800

    adding managed home

--- site/profile/manifests/pr_svcscan.pp
+++ site/profile/manifests/pr_svcscan.pp
@@ -61,11 +61,12 @@ else
     # set password to locked
     # Remotelogin is already set in remotelogin profile for all valid scenarios
     user { 'svcscan':
-      uid      => '32555',
-      gid      => 'svcscan',
-      groups   => 'remotelogin',
-      password => '*LK*',
-      require  => Group['svcscan','remotelogin'],
+      uid        => '32555',
+      gid        => 'svcscan',
+      groups     => 'remotelogin',
+      password   => '*LK*',
+      managehome => true,
+      require    => Group['svcscan','remotelogin'],
     }
   }
 
@@ -85,7 +86,7 @@ else
       ensure  => present,
       user    => 'svcscan',
       type    => 'ssh-rsa',
-      options => ['from="100.64.33.30,100.70.15.158,100.70.15.254,100.70.22.62,142.174.129.135,142.174.129.139,142.174.129.175,142.174.61.117,142.174.61.79,142.178.174.10,142.178.204.112,142.178.204.113,142.178.205.169,142.178.22.16,142.178.22.77,142.63.129.17,204.191.153.3,209.29.0.169,209.29.0.170,209.29.0.44,75.153.176.179,75.153.235.98,75.153.235.99,96.1.170.237,96.1.250.230"','no-agent-forwarding','no-port-forwarding','no-X11-forwarding'],
+      options => ['from="142.178.205.169,142.178.204.112,142.178.174.10,142.174.129.135,142.174.129.139,142.174.129.176,142.174.61.117,142.63.129.17,142.174.61.79,75.153.176.179,96.1.160.14,96.1.166.109,100.77.164.23,100.77.165.23,100.125.253.244 ,100.125.162.244,100.70.15.254,100.64.33.30,100.70.23.30,100.70.15.158,100.70.22.62,96.1.250.230,100.70.30.85,100.70.32.85,204.191.153.3,209.29.0.44,209.29.0.161,209.29.0.169,96.1.170.47,96.1.160.113,142.178.22.16 ,100.70.31.235,205.206.214.190,209.91.119.50,100.125.167.228,100.125.167.244"','no-agent-forwarding','no-port-forwarding','no-X11-forwarding'],
       key     => 'AAAAB3NzaC1yc2EAAAABJQAAAQEAjHK8a6LfhEsrUuy5LxcmgFS++lzLNgnyBoQS8oRsG/Wb8vgbyJ9wW4wLxvTHIOvULsRoR5Q0zrI9uX7NVZug614iZf0qQ4yxrOgkz8Q+bZDO6pbJdFWPyqQP83N7e/1Pk1RULbH7tfe7bD7N0lG0HBy7NMh3rjqISTxB7zHbmM3HXbFFOJs3qqdxZ0foXNpeB9Ob1Plg02B6+fSZ+BeaVNC/zEgMwIxEcgpwq1cMigY+jFlFD6yaArr67wF7WQeArd6f/NY5jq+BEnqhEDp49Yul3naMvuamn52/q46+b4JtsWpL750/c8CRtSDg1LPsrb6jq7HrRK0saqc0o+okuw==',
       require => File['/home/svcscan'],
     }
