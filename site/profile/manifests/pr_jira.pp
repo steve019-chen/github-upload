07b4c814c255ba59b228317f7833cab10a6bf838
Daruvin Sood <daruvin.sood@telus.com>
2019-12-10 11:26:50 -0800

    adding file directly in profile and initd code

--- site/profile/manifests/pr_jira.pp
+++ site/profile/manifests/pr_jira.pp
@@ -32,25 +32,50 @@ group { 'users':
 # }
 
 # Adding Sudo rules for docker and apache
-# Do not change below 2 options, or original sudo file will get deleted
 
+# Do not change below 2 options, or original sudo file will get deleted
 class { 'sudo':
   purge               => false,
   config_file_replace => false,
-  }
+}
 
 # Include rules in “content”
 
- sudo::conf { 'puppet_jira':
-   priority => 10,
-   content  => 'svc_jira ALL=NOPASSWD : /sbin/service httpd start, /sbin/service httpd stop, /sbin/service httpd restart, /sbin/service httpd status, /usr/sbin/chkconfig httpd on, /usr/sbin/chkconfig httpd off, /opt/puppetlabs/bin/puppet agent -t , /opt/puppetlabs/bin/puppet agent -t --debug',
+sudo::conf { 'puppet_jira':
+  priority => 10,
+  content  => 'svc_jira ALL=NOPASSWD : /sbin/service httpd start, /sbin/service httpd stop, /sbin/service httpd restart, /sbin/service httpd status, /usr/sbin/chkconfig httpd on, /usr/sbin/chkconfig httpd off, /opt/puppetlabs/bin/puppet agent -t , /opt/puppetlabs/bin/puppet agent -t --debug',
+}
+sudo::conf { 'puppet_agent':
+  priority => 20,
+  content  => 'svc_jira ALL=NOPASSWD : /opt/puppetlabs/bin/puppet agent -t , /opt/puppetlabs/bin/puppet agent -t --debug',
+}
+
+file {'/etc/rc.d/init.d/test1':
+  ensure  => present,
+  content => "Hello Jojos",
   }
 
-  file {'/etc/rc.d/init.d/test1':
-    ensure => present,
-    content => "Hello Jojos",
-   }
+case $facts['os']['release']['major']  {
+  '5': { $provider = 'redhat' }
+  '6': { $provider = 'redhat' }
+  '7': { $provider = 'redhat' }   #switch to using systemd with a unit file
+  default:   { $provider = 'redhat' }
+}
 
+file { '/etc/init.d/TELUS_jira_control':
+  ensure => 'present',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+  source => 'puppet:///modules/profile/jira/TELUS_jira_control',
+  notify => Service['TELUS_jira_control'],
+}
+
+service { 'TELUS_jira_control':
+  enable    => true,
+  hasstatus => false,
+  provider  => $provider,
+}
 }
 
 # lint:endignore
