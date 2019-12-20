9649e20c852a4a30d0b2e8ba9c1dea81a0288799
Corey Sprung <COREY.SPRUNG@TELUS.COM>
2019-12-20 14:24:40 -0500

    added If

--- site/profile/manifests/pr_perform_upgrade.pp
+++ site/profile/manifests/pr_perform_upgrade.pp
@@ -28,85 +28,90 @@ String  $hostname     = $facts['hostname'],
 
 # Required user uid = 3181(svcbmcp) gid = 3181(bmc) groups = 3181(bmc) context = unconfined_u:unconfined_r:unconfined_t:s0-s0:c0.c1023
 # Create the users group if it doesnt already exsist
-group { 'bmc':
-  ensure => present,
-  gid    => '3181',
-}
+# group { 'bmc':
+#   ensure => present,
+#   gid    => '3181',
+# }
 
 # Create the user for application account if it doesnt already exsist
-user { 'svcbmcp':
-  uid     => '3181',
-  gid     => 'bmc',
-  shell   => '/bin/bash',
-  require => Group['bmc'],
-}
+# user { 'svcbmcp':
+#   uid     => '3181',
+#   gid     => 'bmc',
+#   shell   => '/bin/bash',
+#   require => Group['bmc'],
+# }
 
-
-if (Float.new($facts['os']['release']['full']) >= 6.7)
+if ($facts['perform_info']['installed'] = true)
 {
-# Agent 11.5.01 - 3700 servers in telus greater or equal to 6.7 
-  $installtar = 'TSCO-perform-linux-latest.tar'
-  $installdir = 'TSCO-perform-linux-latest'
-}
-elsif (Float.new($facts['os']['release']['full']) >= 5.2 and Float.new($facts['os']['release']['full']) < 6.7)
-{
-# Agent 10.5.00  - 750 servers in telus between 5.2 and 6.6
-  $installtar = 'TSCO-perform-linux-legacy.tar'
-  $installdir = 'TSCO-perform-linux-legacy'
-}
-else {
-  # lint: ignore: 140chars
-  notify{
-    'Unsupported version of linux OS':,
+  if (Float.new($facts['os']['release']['full']) >= 6.7)
+  {
+  # Agent 11.5.01 - 3700 servers in telus greater or equal to 6.7 
+    $installtar = 'TSCO-perform-linux-latest.tar'
+    $installdir = 'TSCO-perform-linux-latest'
   }
-  #Force an error at runtime
-  exec{'perform_upgrade_not_supported_os':
-    command => '/bin/false',
+  elsif (Float.new($facts['os']['release']['full']) >= 5.2 and Float.new($facts['os']['release']['full']) < 6.7)
+  {
+  # Agent 10.5.00  - 750 servers in telus between 5.2 and 6.6
+    $installtar = 'TSCO-perform-linux-legacy.tar'
+    $installdir = 'TSCO-perform-linux-legacy'
+  }
+  else {
+    # lint: ignore: 140chars
+    notify{
+      'Unsupported version of linux OS':,
+    }
+    #Force an error at runtime
+    exec{'perform_upgrade_not_supported_os':
+      command => '/bin/false',
+    }
+  }
+
+  $patrol = $facts['patrol_info']
+
+  if $space_needed > $patrol['var_tmp_bytes'] {
+    # lint: ignore: 140chars
+    notify{
+      "Filesystem ${patrol['var_tmp_fs']} too full. Need ${space_needed} bytes in /var/tmp but only ${patrol['var_tmp_bytes']} available":,
+    }
+    #Force an error at runtime
+    exec{'perfom_upgrade_no_space':
+      command => '/bin/false',
+    }
+  }
+  else {
+      # download the TAR file and extract into the installdir.
+      archive { "/var/tmp/${installtar}":
+        ensure        => present,
+        source        => "puppet:///software/perform_upgrade/${installtar}",
+        extract       => true,
+        creates       => "/var/tmp/${installdir}",
+        extract_path  => '/var/tmp/',
+        extract_flags => 'xvf',
+        cleanup       => true,
+      }
+
+      # Perfom the installation using the provide telusinstall.sh.
+      exec {'performupgrade':
+        command     => "/var/tmp/${installdir}/telusinstall.sh",
+        path        => ['/sbin','/bin','/usr/sbin','/usr/bin'],
+        cwd         => "/var/tmp/${installdir}",
+        environment => ['HOME=/home/svcbmcp'],
+        creates     => "/tmp/TSCO_${hostname}_Install.txt",
+        timeout     => 3600,
+        require     => Archive["/var/tmp/${installtar}"],
+      }
+
+      # We have already completed, make sure we clean up the directory.
+      tidy {"/var/tmp/${installdir}/":
+        path    => "/var/tmp/${installdir}/",
+        backup  => false,
+        recurse => true,
+      }
+    }
+  }
+  else {
+    #do nothing
   }
 }
 
-$patrol = $facts['patrol_info']
-
-if $space_needed > $patrol['var_tmp_bytes'] {
-  # lint: ignore: 140chars
-  notify{
-    "Filesystem ${patrol['var_tmp_fs']} too full. Need ${space_needed} bytes in /var/tmp but only ${patrol['var_tmp_bytes']} available":,
-  }
-  #Force an error at runtime
-  exec{'perfom_upgrade_no_space':
-    command => '/bin/false',
-  }
-}
-else {
-
-  # download the TAR file and extract into the installdir.
-  archive { "/var/tmp/${installtar}":
-    ensure        => present,
-    source        => "puppet:///software/perform_upgrade/${installtar}",
-    extract       => true,
-    creates       => "/var/tmp/${installdir}",
-    extract_path  => '/var/tmp/',
-    extract_flags => 'xvf',
-    cleanup       => true,
-  }
-
-  # Perfom the installation using the provide telusinstall.sh.
-  exec {'performupgrade':
-    command     => "/var/tmp/${installdir}/telusinstall.sh",
-    path        => ['/sbin','/bin','/usr/sbin','/usr/bin'],
-    cwd         => "/var/tmp/${installdir}",
-    environment => ['HOME=/home/svcbmcp'],
-    creates     => "/tmp/TSCO_${hostname}_Install.txt",
-    timeout     => 3600,
-    require     => Archive["/var/tmp/${installtar}"],
-  }
-
-  # We have already completed, make sure we clean up the directory.
-  tidy {"/var/tmp/${installdir}/":
-    path    => "/var/tmp/${installdir}/",
-    backup  => false,
-    recurse => true,
-  }
-}
-}
 # lint: endignore
