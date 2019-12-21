42fb9e341447c4eea99d2b6c804c44dc5bc95ca8
Daruvin Sood <daruvin.sood@telus.com>
2019-12-20 23:24:13 -0800

    adding spec testing for perform upgrade

---
+++ spec/classes/pr_perform_upgrade_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe 'profile::pr_perform_upgrade' do
+  let(:title) { 'pr_perform_upgrade' }
+  let(:node) { 'test.example.com' }
+  let(:facts) do
+    {
+      'os' => {
+        'family' => 'RedHat',
+        'release' => {
+          'major' => '5',
+          'minor' => '3',
+          'full' => '5.3',
+        },
+      },
+      'patrol_info' => {
+        'var_tmp_bytes' => 310200001,
+        'var_tmp_fs' => '/var/tmp',
+        },
+        'perform_info' => {
+          'installed' => 'False',
+          'var_tmp_fs' => '/var/tmp',
+          },
+    }
+  end
+
+  it { is_expected.to compile.with_all_deps }
+ # it { is_expected.to contain_file('var/tmp/TSCO-perform-linux-legacy') }
+
+end
