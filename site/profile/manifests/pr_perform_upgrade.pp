3fa52a9b0e2ef478e74028915c2ac470b59df1c7
Corey Sprung <COREY.SPRUNG@TELUS.COM>
2019-12-30 14:09:47 -0500

    updated source

--- site/profile/manifests/pr_perform_upgrade.pp
+++ site/profile/manifests/pr_perform_upgrade.pp
@@ -209,7 +209,7 @@ $best1home    = $facts['perform_info']['best1home'],
           # download the TAR file and extract into the installdir.
           archive { "/var/tmp/${installtar}":
             ensure        => present,
-            source        => "puppet:///software/perform_upgrade/${installtar}",
+            source        => "http://lp99850.corp.ads/downloads/linux/${installtar}",
             extract       => true,
             creates       => "/var/tmp/${installdir}",
             extract_path  => '/var/tmp/',
