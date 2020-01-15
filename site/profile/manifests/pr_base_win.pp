e311f57ad174b39e36ce0ec91c077c420f02d580
Daruvin Sood <daruvin.sood@telus.com>
2020-01-15 14:45:41 -0800

    removing extra character

--- site/profile/manifests/pr_base_win.pp
+++ site/profile/manifests/pr_base_win.pp
@@ -21,7 +21,7 @@ class profile::pr_base_win {
 
 # # Limiting deployment to : SDE, KIDC Non Prod, QIDC NP, Laird, Toll, KIDC Prod
 # # Not deploying to: QIDC Prod, QIDC Tools, KIDC Tools,
-if ($facts['puppet_server'] in ['btln007206.corp.ads','btln002494.corp.ads','btln000197.corp.ads','lp99604.corp.ads','lp99605.corp.ads​','btlp000336.corp.ads'] )
+if ($facts['puppet_server'] in ['btln007206.corp.ads','btln002494.corp.ads','btln000197.corp.ads','lp99604.corp.ads','lp99605.corp.ads','btlp000336.corp.ads'] )
   { include profile::pr_base_win_temp }
 
 }
