a3c1466d1bb43d97d2293ac067f2d8c5eb980832
Sharanya Raja <sharanya.raja@telus.com>
2020-01-20 15:09:45 -0800

    Code added

--- site/profile/manifests/pr_static_route.pp
+++ site/profile/manifests/pr_static_route.pp
@@ -13,4 +13,24 @@
 # Sharanya
 class profile::pr_example {
 # Comment
+#Add version lock to docker package
+package {'yum-plugin-versionlock':
+  ensure => present,
+}
+
+exec { 'yum versionlock docker-ce':
+  path    => '/bin:/usr/bin:/usr/sbin:/bin',
+  unless  => 'cat /etc/yum/pluginconf.d/versionlock.list | grep -q docker-ce > /dev/null',
+  require => Package['yum-plugin-versionlock'],
+}
+
+file_line { 'yum_versionlock_config':
+  ensure             => present,
+  path               => '/etc/yum/pluginconf.d/versionlock.conf',
+  line               => 'show_hint = 0',
+  match              => 'show_hint = 1',
+  append_on_no_match => false,
+  require            => Package['yum-plugin-versionlock'],
+}
+
 }
