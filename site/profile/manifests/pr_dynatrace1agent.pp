d5f5deb90d92c074d25ebd945c0be022a21d291b
Shaun McEvoy <Shaun.McEvoy@telus.com>
2020-06-04 00:27:46 -0600

    add new sudo rules

--- site/profile/manifests/pr_dynatrace1agent.pp
+++ site/profile/manifests/pr_dynatrace1agent.pp
@@ -71,7 +71,7 @@ class profile::pr_dynatrace1agent (
   # Configure sudo rules for dynatrace
   sudo::conf { 'puppet_dynatrace':
     priority => 10,
-    content  => 'dynatrace ALL=NOPASSWD : /opt/puppetlabs/bin/puppet agent -t , /opt/puppetlabs/bin/puppet agent -t --debug, /bin/systemctl stop oneagent, /bin/systemctl start oneagent, /opt/dynatrace/oneagent/agent/uninstall.sh,/bin/rm -rf /opt/dynatrace/oneagent, /bin/rm -rf /var/lib/dynatrace/oneagent/agent/config',
+    content  => 'dynatrace ALL=NOPASSWD : /bin/systemctl stop oneagent, /bin/systemctl start oneagent, /bin/systemctl status oneagent, /usr/sbin/service oneagent stop, /usr/sbin/service oneagent start, /usr/sbin/service oneagent status, /bin/rm -rf /opt/dynatrace/oneagent, /bin/rm -rf /var/lib/dynatrace/oneagent/agent/config',
     }
     # lint:ignore:140chars
     # lint:endignore
