bc3b37e018bdb75f0fedae4522743d8716632573
Corey Sprung <COREY.SPRUNG@TELUS.COM>
2020-08-12 12:50:10 -0400

    changed the timeout to 2400 seconds

--- site/profile/manifests/pr_perform_upgrade.pp
+++ site/profile/manifests/pr_perform_upgrade.pp
@@ -151,7 +151,7 @@ Integer $osversion = Integer.new(($facts['os']['release']['major']) * 100 + ($fa
           path    => ['/sbin','/bin','/usr/sbin','/usr/bin'],
           cwd     => "/var/tmp/${installdir}",
           creates => "/var/tmp/TSCO_${hostname}_Install.txt",
-          timeout => 1200,
+          timeout => 2400,
           require => Exec["untar ${installtar}"],
         }
       }
